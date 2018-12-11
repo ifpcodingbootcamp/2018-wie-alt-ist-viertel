c55c2ddcd0188901a7b364d66067a502d9816cdf
Felix Ebert <felix.ebert@if-core.de>
2018-12-11 16:24:27 +0100

    Als User will ich die Stadtteile eingefaerbt nach Altersdurchschnitt sehen

--- AlterMap.js
+++ AlterMap.js
@@ -1,5 +1,13 @@
+var colors = ['#edf8fb', '#ccece6', '#99d8c9', '#66c2a4', '#2ca25f', '#006d2c'];
+
 //Karte initialisieren, Zentrum und Zoom setzen
-var map = L.map('mapid').setView([48.137, 11.576], 10);
+var map = L.map('mapid',
+    {
+        minZoom: 9,
+        maxZoom: 12,
+        zoomDelta: 0.1,
+        zoomSnap: 0 //pinch to zoom Mobiles Zoomen
+    });
 //Hintergrund Weltkarte
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -13,11 +21,54 @@ map.fitBounds([
 ]);
 //Stadtteile zeichnen
 L.geoJson(stadtteile, {
-    style: function (feature) {
+    style: function (Stadtteil) {
         return {
-            fillColor:"transparent",
-            color:"#333333",
-            weight:1.5
+            fillColor: getcolor(Stadtteil), //Stadtteile einfärben - siehe fct getcolor
+            color: "#333333",
+            weight: 1.5,
+            fillOpacity: 1
         }
     }
 }).addTo(map);
+
+
+function getcolor(Stadtteil) {
+
+    var Stadtteilname = Stadtteil.properties.name //Aufruf stadtteile.js
+
+    var value = getAlter(Stadtteilname)
+
+    if (value <= 39) { //Bedingungen für Farbe definieren
+        return colors [0]
+    }
+    if (value <= 40) {
+        return colors [1]
+    }
+    if (value <= 41) {
+        return colors [2]
+    }
+    if (value <= 42) {
+        return colors [3]
+    }
+    if (value <= 43) {
+        return colors [4]
+    }
+    if (value <= 44) {
+        return colors [5]
+    }
+
+
+}
+
+function getAlter(Stadtteilname) { //Schleifen-Funktion für Altersdurchschnitt
+
+    for (var i = 0; i < altersdurchschnitt.length; i++) {
+//Bezug zu altersdurschnitt.js
+        if (Stadtteilname.startsWith(altersdurchschnitt[i].Stadtbezirk)) {
+
+            return altersdurchschnitt[i].Indikator
+        }
+
+    }
+
+}
