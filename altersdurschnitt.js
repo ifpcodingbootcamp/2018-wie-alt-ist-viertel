342c5caf721af21fe3a70c48bfa3244e354534f6
Felix Ebert <felix.ebert@if-core.de>
2018-12-11 14:00:38 +0100

    stadtteile und altersdurchschnitt

---
+++ altersdurschnitt.js
@@ -0,0 +1,152 @@
+var altersdurchschnitt = [
+    {
+        "Stadtbezirk": "Stadtbezirk 10",
+        "Stadtteilnummer": 10,
+        "Name": "10 Moosach",
+        "Indikator": 41.4
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 11",
+        "Stadtteilnummer": 11,
+        "Name": "11 Milbertshofen - Am Hart",
+        "Indikator": 39.6
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 12",
+        "Stadtteilnummer": 12,
+        "Name": "12 Schwabing - Freimann",
+        "Indikator": 39.3
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 13",
+        "Stadtteilnummer": 13,
+        "Name": "13 Bogenhausen",
+        "Indikator": 42.9
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 14",
+        "Stadtteilnummer": 14,
+        "Name": "14 Berg am Laim",
+        "Indikator": 41.1
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 15",
+        "Stadtteilnummer": 15,
+        "Name": "15 Trudering - Riem",
+        "Indikator": 39.8
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 16",
+        "Stadtteilnummer": 16,
+        "Name": "16 Ramersdorf - Perlach",
+        "Indikator": 42.1
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 17",
+        "Stadtteilnummer": 17,
+        "Name": "17 Obergiesing - Fasangarten",
+        "Indikator": 40.2
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 18",
+        "Stadtteilnummer": 18,
+        "Name": "18 Untergiesing - Harlaching",
+        "Indikator": 42.8
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 19",
+        "Stadtteilnummer": 19,
+        "Name": "19 Thalkirchen - Obersendling - Forstenried - Fürstenried - Solln",
+        "Indikator": 43.1
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 20",
+        "Stadtteilnummer": 20,
+        "Name": "20 Hadern",
+        "Indikator": 43.2
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 21",
+        "Stadtteilnummer": 21,
+        "Name": "21 Pasing - Obermenzing",
+        "Indikator": 42.3
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 22",
+        "Stadtteilnummer": 22,
+        "Name": "22 Aubing - Lochhausen - Langwied",
+        "Indikator": 41.6
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 23",
+        "Stadtteilnummer": 23,
+        "Name": "23 Allach - Untermenzing",
+        "Indikator": 42.1
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 24",
+        "Stadtteilnummer": 24,
+        "Name": "24 Feldmoching - Hasenbergl",
+        "Indikator": 41.4
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 25",
+        "Stadtteilnummer": 25,
+        "Name": "25 Laim",
+        "Indikator": 42.2
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 01",
+        "Stadtteilnummer": "01",
+        "Name": "01 Altstadt - Lehel",
+        "Indikator": 41.7
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 02",
+        "Stadtteilnummer": "02",
+        "Name": "02 Ludwigsvorstadt - Isarvorstadt",
+        "Indikator": 39
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 03",
+        "Stadtteilnummer": "03",
+        "Name": "03 Maxvorstadt",
+        "Indikator": 38.6
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 04",
+        "Stadtteilnummer": "04",
+        "Name": "04 Schwabing - West",
+        "Indikator": 40.8
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 05",
+        "Stadtteilnummer": "05",
+        "Name": "05 Au - Haidhausen",
+        "Indikator": 40.1
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 06",
+        "Stadtteilnummer": "06",
+        "Name": "06 Sendling",
+        "Indikator": 40.4
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 07",
+        "Stadtteilnummer": "07",
+        "Name": "07 Sendling - Westpark",
+        "Indikator": 41.4
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 08",
+        "Stadtteilnummer": "08",
+        "Name": "08 Schwanthalerhöhe",
+        "Indikator": 39.1
+    },
+    {
+        "Stadtbezirk": "Stadtbezirk 09",
+        "Stadtteilnummer": "09",
+        "Name": "09 Neuhausen - Nymphenburg",
+        "Indikator": 41.2
+    }
+]
